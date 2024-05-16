b2a61868127a0dcaa681f334a9ac1064400d3c4f
Oanasuciu97 <luciana_bodeaoana@yahoo.com>
2024-05-16 12:21:49 +0300

    src update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,2 +1,13 @@
 export const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '26682637 - dbd8b52f010b530b6481286b3';
+export const options = {
+  params: {
+    key: API_KEY,
+    q: '',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: 1,
+    per_page: 40,
+  },
+};
